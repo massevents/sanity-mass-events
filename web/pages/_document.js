bbce7f6166037f3387433fb4ee739d16432266b2
Robin Heij <rooobiin89@gmail.com>
2020-11-16 20:03:18 +0100

    feat: add GA and facebook pixel

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -32,6 +32,44 @@ export default class MyDocument extends Document {
     return (
       <Html lang={"nl"}>
         <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-8K95CDYYZW"></script>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-8K95CDYYZW');`,
+            }}
+          />
+
+<script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1355490448116285');
+              fbq('track', 'PageView');`,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+              <img
+                height="1"
+                width="1"
+                style="display:none"
+                src="https://www.facebook.com/tr?id=1355490448116285&ev=PageView&noscript=1"
+              />`,
+            }}
+          />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Rubik:wght@400;700&display=swap"
             rel="stylesheet"
