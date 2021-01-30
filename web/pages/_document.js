d80a4bf81d67ce22aac7d5b8ee6244f4e72a42aa
Robin Heij <rooobiin89@gmail.com>
2021-01-30 23:51:40 +0100

    fix: add gtm

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -32,19 +32,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang={"nl"}>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-8K95CDYYZW"></script>
 
           <script
             dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', 'G-8K95CDYYZW');`,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5KZXFCR');`,
             }}
           />
 
-<script
+          <script
             dangerouslySetInnerHTML={{
               __html: `!function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -77,6 +76,15 @@ export default class MyDocument extends Document {
         </Head>
 
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5KZXFCR"
+              height="0"
+              width="0"
+              style={{display:'none',visibility:'hidden'}}
+            ></iframe>
+          </noscript>
+
           <Main />
           <NextScript />
         </body>
